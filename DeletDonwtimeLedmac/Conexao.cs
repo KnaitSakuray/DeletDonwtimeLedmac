2dac4a170faa3380f2e0392369293314b8933c5b
Eduardo Lopez <88441218+KnaitSakuray@users.noreply.github.com>
2022-10-14 10:22:57 -0300

    Update Conexao.cs

--- DeletDonwtimeLedmac/Conexao.cs
+++ DeletDonwtimeLedmac/Conexao.cs
@@ -10,7 +10,7 @@ namespace DeletDonwtimeLedmac
     class Conexao
     {
         //encapsulamento para chegar no banco
-        private static string _ROTA = @"Data Source = ext-sfis-dev\sqlexpress; Initial Catalog = LEDMEC; Persist Security Info=True;User ID =UserDeleteSistemas; Password=Go9W1QZ1";
+        private static string _ROTA = @"";
         public static string ROTA
         {
             get { return _ROTA; }
